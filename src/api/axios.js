81b31b60840fee4c84bd551d72757046b071a437
Mlamli Lolwane <mrlolwane96@gmail.com>
2026-02-26 02:52:07 +0200

    update backend endpoint for axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5278/api", // your ASP.NET API base URL
+  baseURL: "https://player2.mrlolwane96.workers.dev/api", // your ASP.NET API base URL
   headers: {
     "Content-Type": "application/json"
   }
